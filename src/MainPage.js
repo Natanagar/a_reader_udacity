75e0af71aeb5029647f6545624bc8bde7a09b9da
Natalia Agarkowa <agarkowa@gmail.com>
2018-07-21 21:17:03 +0200

    add react function

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,9 +4,60 @@ import Book from './Book'
 class MainPage extends Component{
     render(){
         return(
-            <div>Hello World!
-                <Book />
+            <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
             </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      <li>
+                        <Book />
+                      </li>
+                      <li>
+                        <Book />
+                      </li>
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      <li>
+                          <Book />
+                      </li>
+                      <li>
+                        <Book />
+                      </li>
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      <li>
+                        <Book />
+                      </li>
+                      <li>
+                        <Book />
+                      </li>
+                      <li>
+                        <Book />
+                      </li>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
+          </div>
         );
     }
 }
